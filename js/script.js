4dac112ead11b16f422cc113d45e424d6fa07adf
DianaBabenko <baben.k.o.diana2017@gmail.com>
2019-11-23 14:02:52 +0200

    optimize

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ if (localStorageString !== null) {
 }
 
 function addProduct() {
-    const productName = document.getElementById('inputName');
-    const name = productName.value;
-    const quantityOfProduct = document.getElementById('inputQuantity');
-    const quantity = parseInt(quantityOfProduct.value);
+    const inputName = document.getElementById('inputName');
+    const name = inputName.value;
+    const inputQuantity = document.getElementById('inputQuantity');
+    const quantity = parseInt(inputQuantity.value);
 
     const product = new Product(name, quantity);
     if (name && quantity) {
@@ -30,9 +30,9 @@ function removeProduct(index) {
 }
 
 function searchByName(value) {
-    let a = products.indexOf(products.find(item => item.name === value));
-    alert("The index of item is: " + a);
-    return a;
+    let itemIndex = products.indexOf(products.find(item => item.name === value));
+    alert("The index of item is: " + itemIndex);
+    return itemIndex;
 }
 
 function updateList() {
@@ -42,57 +42,62 @@ function updateList() {
 function generateList() {
     const list = document.createElement('ul');
     list.className = "list-group";
-        for (let productIndex in products) {
-            const productEl = document.createElement('li');
-            const name = document.createElement('span');
-            const quantity = document.createElement('span');
-            const status = document.createElement('span');
-            const deleteProduct = document.createElement('button');
 
-            if (products[productIndex].mark !== 0) {
-                status.innerText = "Sold";
-                status.className = "statusIcon badge badge-danger col-2";
+    for (let productIndex in products) {
+        const productEl = document.createElement('li');
+        const name = document.createElement('span');
+        const quantity = document.createElement('span');
+        const status = document.createElement('span');
+        const deleteProduct = document.createElement('button');
 
-            } else {
-                status.innerText = "Available";
-                status.className = "statusIcon badge badge-success badge-pill col-2 ";
-            }
-
-            name.className = "product col-5";
-            quantity.className = "quantity badge badge-secondary badge-pill col-1";
-
-            deleteProduct.className = "deleteProduct btn btn-outline-warning col-1 sticky-right";
-            productEl.className = "list-group-item d-flex justify-content-between align-items-center row border-warning";
-
-
-            status.id = "statusOfProduct";
-            deleteProduct.id = "deleteProduct";
-            productEl.id = "productEl";
-
-            name.innerText = products[productIndex].name;
-            quantity.innerText = JSON.stringify(products[productIndex].quantity);
-            deleteProduct.innerText = "X";
-
-            productEl.appendChild(name);
-            productEl.appendChild(quantity);
-            productEl.appendChild(status);
-            productEl.appendChild(deleteProduct);
-
-            status.onclick = () => {
-                status.className = "statusIcon badge badge-danger col-2";
-                changeStatus(productIndex);
-            };
-
-            deleteProduct.onclick = () => {
-                removeProduct(productIndex);
-            };
-
-            list.appendChild(productEl);
+        if (products[productIndex].mark !== 0) {
+            status.innerText = "Sold";
+            status.className = "list-group__item_sold badge badge-danger badge-pill col-2";
+        } else {
+            status.innerText = "Available";
+            status.className = "list-group__item_available badge badge-success badge-pill col-2 ";
         }
+
+        name.className = "list-group__item_name col-5";
+        quantity.className = "list-group__item_quantity badge badge-secondary badge-pill col-1";
+        deleteProduct.className = "deleteProduct btn btn-outline-warning col-1 sticky-right";
+        productEl.className = "list-group__item list-group-item d-flex justify-content-between align-items-center row border-warning";
+
+        status.id = "statusOfProduct";
+        deleteProduct.id = "deleteProduct";
+        productEl.id = "productEl";
+
+        name.innerText = products[productIndex].name;
+        quantity.innerText = JSON.stringify(products[productIndex].quantity);
+        deleteProduct.innerText = "X";
+
+        productEl.appendChild(name);
+        productEl.appendChild(quantity);
+        productEl.appendChild(status);
+        productEl.appendChild(deleteProduct);
+
+        status.onclick = () => {
+            if (products[productIndex].mark === 1) {
+                status.innerText = "Available";
+                status.className = "list-group__item_available badge badge-success badge-pill col-2 ";
+                products[productIndex].mark = 0;
+            } else {
+                status.className = "list-group__item_sold badge badge-danger badge-pill col-2";
+                changeStatus(productIndex);
+            }
+        };
+
+        deleteProduct.onclick = () => {
+            removeProduct(productIndex);
+        };
+
+        list.appendChild(productEl);
+    }
     const productsList = document.getElementById('productsList');
     productsList.innerHTML = '';
     productsList.appendChild(list);
     updateList();
+    console.log(products);
 }
 
 function changeStatus(index) {
@@ -115,6 +120,8 @@ function sortForm() {
     listByDefault.innerText = "Random";
     listByDefault.className = "dropdown-item";
 
+    dropdownMenu.style.cursor = "pointer";
+
     sortLowToHigh.onclick = () => {
         products.sort((a, b) => a.quantity > b.quantity ? 1 : -1);
         generateList();
@@ -135,37 +142,37 @@ function sortForm() {
     dropdownMenu.appendChild(listByDefault);
 }
 
-function inputForm() {
+function addForm() {
     const nameLabel = document.createElement('label');
     nameLabel.innerText = "Name";
-    nameLabel.className = "nameLabel mr-2";
+    nameLabel.className = "add-form__label-name mr-2";
 
-    const inputName = document.createElement('input');
-    inputName.type = "text";
-    inputName.id = "inputName";
-    inputName.className = "form-control";
+    const nameInput = document.createElement('input');
+    nameInput.type = "text";
+    nameInput.id = "inputName";
+    nameInput.className = "add-form__input-name form-control";
 
     const quantityLabel = document.createElement('label');
     quantityLabel.innerText = "Quantity";
-    quantityLabel.className = "quantityLabel ml-3 pr-2";
+    quantityLabel.className = "add-form__label-quantity ml-3 pr-2";
 
     const inputQuantity = document.createElement('input');
     inputQuantity.type = "text";
     inputQuantity.id = "inputQuantity";
-    inputQuantity.className = "form-control";
+    inputQuantity.className = "add-form__input-quantity form-control";
 
     const addButton = document.getElementById('addButton');
     addButton.onclick = () => {
         addProduct();
-        inputName.value = '';
+        nameInput.value = '';
         inputQuantity.value ='';
     };
 
     const addForm = document.getElementById('addForm');
-    addForm.className = "form-group form-inline";
+    addForm.className = "add-form form-group form-inline";
 
     addForm.appendChild(nameLabel);
-    addForm.appendChild(inputName);
+    addForm.appendChild(nameInput);
     addForm.appendChild(quantityLabel);
     addForm.appendChild(inputQuantity);
 }
@@ -175,10 +182,10 @@ function searchForm() {
     searchInput.type = "text";
     searchInput.id = "inputSearch";
     searchInput.placeholder = "Search";
-    searchInput.className = "form-control";
+    searchInput.className = "search-form__input form-control";
 
     const searchButton = document.createElement('button');
-    searchButton.className = "searchButton btn btn-primary ml-1";
+    searchButton.className = "search-form__button btn btn-primary ml-1";
     searchButton.innerText = "Find";
 
     searchButton.onclick = () => {
@@ -187,11 +194,11 @@ function searchForm() {
     };
 
     const searchForm = document.getElementById('searchForm');
-    searchForm.className = "form-group has-search";
+    searchForm.className = "search-form position-relative text-center has-search";
     searchForm.appendChild(searchInput);
     searchForm.appendChild(searchButton);
 }
 searchForm();
-inputForm();
+addForm();
 sortForm();
 generateList();
